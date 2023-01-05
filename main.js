88828c3beba2485d5867cf16965d835a12f9e944
MineKir <vinhhttt@gmail.com>
2023-01-05 23:13:05 +0700

    upload

--- main.js
+++ main.js
@@ -1,31 +1,83 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("intro-slide");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
-}
+// function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("intro-slide");
+//     var dots = document.getElementsByClassName("dot");
+//     if (n > slides.length) {
+//         slideIndex = 1
+//     }
+//     if (n < 1) {
+//         slideIndex = slides.length
+//     }
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+//     dots[slideIndex - 1].className += " active";
+//     setTimeout(showSlides, 4000); // Change image every 4 seconds
+// }
+
+// let click = document.querySelector('.text').innerHTML="<h3>baby</h3>";
+// click.onclick = function(){
+//     click.classList.add('active');
+// }
+// const car={
+//     name:'Vinh',
+//     age:18
+// }
+// console.log(car.name);
+
+// const promise = new Promise((resolve, reject) => {
+//     httpGetAsync('htps://picsum.photos/200/300',resolve);
+// });
+// promise.then(data) = function() {
+//     console.log(data);
+// };
+// async
+
+
+// Api du bao thoi tiet
+const APP_ID = 'cf26e7b2c25b5acd18ed5c3e836fb235';
+const DEFAULT_VALUE = '--';
+const searchInput = document.querySelector('#search-input');
+const cityName = document.querySelector('.city-name');
+const weatherState = document.querySelector('.weather-state');
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+
+
+const sunrise = document.querySelector('.sunrise');
+const sunset = document.querySelector('.sunset');
+const humidity = document.querySelector('.humidity');
+const windSpeed = document.querySelector('.wind-speed');
+
+
+searchInput.addEventListener('change', (e) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}&units=metric&lang=vi`)
+        .then(async res => {
+            const data = await res.json();
+            console.log('[Search Input]', data);
+            cityName.innerHTML = data.name || DEFAULT_VALUE;
+            weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
+            weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+            temperature.innerHTML = Math.round(data.main.temp) || DEFAULT_VALUE;
+
+            sunrise.innerHTML = moment.unix(data.sys.sunrise).format('H:mm') || DEFAULT_VALUE;
+            sunset.innerHTML = moment.unix(data.sys.sunset).format('H:mm') || DEFAULT_VALUE;
+            humidity.innerHTML = data.main.humidity || DEFAULT_VALUE;
+            windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE;
+        });
+});
